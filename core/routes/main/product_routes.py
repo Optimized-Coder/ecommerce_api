250cf4d4fc70a39219aabaea68856eadba2abe3a
[Josh Shepherd] <[optimizedcoder.py@gmail.com]>
2023-02-23 23:24:43 +0000

    add logic to filter by genre

--- core/routes/main/product_routes.py
+++ core/routes/main/product_routes.py
@@ -4,18 +4,37 @@ from ... import db
 
 from flask import render_template
 
+genres = set()
 
+def get_genres():
+    for record in Product.query.all():
+        genres.add(record.genre)
+    return genres
 
 @product.route('/')
 def index():
-    genres = set()
-    for record in Product.query.all():
-        genres.add(record.genre)
-    
+    get_genres()
+
     context = {
         'title': 'Products | Book Bros',
         'Product': Product,
         'records': Product.query.all(),
+        'genres': genres
+    }
+
+    return render_template(
+        'product_list.html',
+        **context
+    )
+
+@product.route('/<genre_tag>/')
+def by_genre(genre_tag):
+    get_genres()
+
+    context = {
+        'title': 'Products | Book Bros',
+        'Product': Product,
+        'records': Product.query.filter_by(genre=genre_tag.title()).all(),
         'genres': genres
     }
 
