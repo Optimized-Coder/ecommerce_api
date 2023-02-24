1615ef6cd2133887c08693075cb46a7df9c317b8
[Josh Shepherd] <[optimizedcoder.py@gmail.com]>
2023-02-24 11:45:05 +0000

    add sorting function on product routes

--- core/routes/main/product_routes.py
+++ core/routes/main/product_routes.py
@@ -2,23 +2,30 @@ from . import bp_2 as product
 from ...models.products import Product
 from ... import db
 
-from flask import render_template
+from flask import render_template, request
 
 genres = set()
 
 def get_genres():
-    for record in Product.query.all():
+    records = Product.query.all()
+    for record in records:
         genres.add(record.genre)
     return genres
+
+
 
 @product.route('/')
 def index():
     get_genres()
 
+    sort = request.args.get('sort')
+
     context = {
         'title': 'Products | Book Bros',
         'Product': Product,
-        'records': Product.query.all(),
+        'records': Product.query
+        .order_by(sort)
+        .all(),
         'genres': genres
     }
 
@@ -29,12 +36,18 @@ def index():
 
 @product.route('/<genre_tag>/')
 def by_genre(genre_tag):
+
+    sort = request.args.get('sort')
+    
     get_genres()
 
     context = {
         'title': 'Products | Book Bros',
         'Product': Product,
-        'records': Product.query.filter_by(genre=genre_tag.title()).all(),
+        'records': Product.query
+        .order_by(sort)
+        .filter_by(genre=genre_tag.title())
+        .all(),
         'genres': genres
     }
 
